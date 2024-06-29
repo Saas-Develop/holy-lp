6d2e05540573349758595d1d5bb6ee821bc62f46
alexandresaints <alexandresaintss@gmail.com>
2024-06-28 22:51:00 -0300

    a

--- src/components/Pixel/GooglePixel.js
+++ src/components/Pixel/GooglePixel.js
@@ -2,16 +2,16 @@
 
 import React, { useEffect } from "react";
 
-const GoogleTagManager = () => {
+const Analytics = () => {
   useEffect(() => {
     // Adiciona o script do gtag.js ao documento
-    const script = document.createElement("script");
-    script.src = "https://www.googletagmanager.com/gtag/js?id=AW-16628924935";
-    script.async = true;
-    document.head.appendChild(script);
+    const gtagScript = document.createElement("script");
+    gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=AW-16628924935";
+    gtagScript.async = true;
+    document.head.appendChild(gtagScript);
 
     // Adiciona a configuração do gtag
-    script.onload = () => {
+    gtagScript.onload = () => {
       window.dataLayer = window.dataLayer || [];
       function gtag() {
         window.dataLayer.push(arguments);
@@ -20,13 +20,51 @@ const GoogleTagManager = () => {
       gtag('config', 'AW-16628924935');
     };
 
-    // Remove o script quando o componente é desmontado
+    // Adiciona o Meta Pixel ao documento
+    !(function (f, b, e, v, n, t, s) {
+      if (f.fbq) return;
+      n = f.fbq = function () {
+        n.callMethod
+          ? n.callMethod.apply(n, arguments)
+          : n.queue.push(arguments);
+      };
+      if (!f._fbq) f._fbq = n;
+      n.push = n;
+      n.loaded = !0;
+      n.version = "2.0";
+      n.queue = [];
+      t = b.createElement(e);
+      t.async = !0;
+      t.src = v;
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s);
+    })(
+      window,
+      document,
+      "script",
+      "https://connect.facebook.net/en_US/fbevents.js"
+    );
+    fbq("init", "793022042614450"); // Substitua pelo seu Pixel ID
+    fbq("track", "PageView");
+
+    // Adiciona a tag de imagem de fallback do Meta Pixel
+    const noscript = document.createElement("noscript");
+    const img = document.createElement("img");
+    img.height = "1";
+    img.width = "1";
+    img.style.display = "none";
+    img.src = "https://www.facebook.com/tr?id=793022042614450&ev=PageView&noscript=1";
+    noscript.appendChild(img);
+    document.body.appendChild(noscript);
+
+    // Remove os scripts quando o componente é desmontado
     return () => {
-      document.head.removeChild(script);
+      document.head.removeChild(gtagScript);
+      document.body.removeChild(noscript);
     };
   }, []);
 
   return null;
 };
 
-export default GoogleTagManager;
+export default Analytics;
